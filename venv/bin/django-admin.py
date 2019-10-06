86400b742bd454d7d9ff99f25f6e9f097e54f2c1
Naledi Madlopha <naledi.madlopha@gmail.com>
2019-10-07 01:24:17 +0200

    Initial commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/naledi/Workspace/practise/Python/google-python-exercises/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
